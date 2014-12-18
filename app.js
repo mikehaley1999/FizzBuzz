5796312d218b3f1bca3b79918dfa150503a55b1b
mikehaley1999 <themichaelhaley@gmail.com>
2014-12-18 16:43:16 -0600

    Finished Fizz Buzz and First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('body').on('click', 'button', function(event) {
+
+/*var i = 1;*/
+
+for (var i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+		$('.fizzbuzz').append("<li>Fizz Buzz</li>");
+	} else if (i % 3 === 0) {
+		$('.fizzbuzz').append("<li>Fizz</li>");
+		} else if (i % 5 === 0) {
+			$('.fizzbuzz').append("<li>Buzz</li>");
+			} else {
+				$('.fizzbuzz').append("<li>" + i + "</li>");
+			}
+	}
+})
+});
